7becbd4dca0e015e647fdc759f91598c1ac07804
DBAN <dban7171@gmail.com>
2021-12-30 11:31:11 +0900

    Feat : Markdown 적용

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -17,15 +17,17 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
-            answer.author = request.user  # author 속성에 로그인 계정 저장
+            answer.author = request.user  # 추가한 속성 author 적용
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=question.id), answer.id))
     else:
         form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
+
 
 @login_required(login_url='common:login')
 def answer_modify(request, answer_id):
@@ -41,13 +43,16 @@ def answer_modify(request, answer_id):
         form = AnswerForm(request.POST, instance=answer)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
     return render(request, 'pybo/answer_form.html', context)
+
 
 @login_required(login_url='common:login')
 def answer_delete(request, answer_id):
